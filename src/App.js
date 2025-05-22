07df620c2f7a0037fdec5d200ca0c7a60234ec2c
VelikaGomes <velikagomes21@gmail.com>
2025-05-22 11:24:44 +0530

    Home Page done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import HomePage from './components/HomePage';
+import UploadReceipt from './components/UploadReceipt';
 function App() {
+  const [step, setStep] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {step === 1 && <HomePage onStart={() => setStep(2)} />}
+      {step ===2 && <UploadReceipt/>}
     </div>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = ({ onStart }) => {
+  return (
+    <div style={{ backgroundColor:'#E1DACA', textAlign: 'center', width:'100%', height:'100vh', padding:'10px' }}>
+      <h1>Scan. Tap. Split.</h1>
+      <p>Snap the receipt, tap your items, see who owes what. No sign-ups, no math, no drama.</p>
+      <button style={{ backgroundColor:'#CC5500', width:'70%', height:'50px', borderRadius:'15px',color:'white', fontSize:'15px', padding:'5px' }}onClick={onStart}>Scan Receipt</button>
+      <br /><br />
+      <button style={{ backgroundColor:'white', width:'70%', height:'50px', borderRadius:'15px',color:'black', fontSize:'15px', padding:'5px' }}>Enter Manually</button>
+    </div>
+  );
+};
+
+export default Home;
